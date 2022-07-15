b081d56a5a8f26f8f1a60823888f9c60f6169b3f
zgcwkj <zgcwkj@139.com>
2022-07-15 11:51:27 +0800

    OneSentence_20181112

---
+++ OneSentence/Main.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace OneSentence
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Txt_hitokoto = new System.Windows.Forms.Label();
+            this.Fd_Style = new System.Windows.Forms.FontDialog();
+            this.Cd_color = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // Txt_hitokoto
+            // 
+            this.Txt_hitokoto.AutoSize = true;
+            this.Txt_hitokoto.Font = new System.Drawing.Font("微软雅黑 Light", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Txt_hitokoto.Location = new System.Drawing.Point(13, 17);
+            this.Txt_hitokoto.Name = "Txt_hitokoto";
+            this.Txt_hitokoto.Size = new System.Drawing.Size(146, 41);
+            this.Txt_hitokoto.TabIndex = 0;
+            this.Txt_hitokoto.Text = "测试文本";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(171, 75);
+            this.Controls.Add(this.Txt_hitokoto);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "一言";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Txt_hitokoto;
+        private System.Windows.Forms.FontDialog Fd_Style;
+        private System.Windows.Forms.ColorDialog Cd_color;
+    }
+}
+
---
+++ OneSentence/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OneSentence
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string hitokoto = "";
+            do
+            {
+                hitokoto = OneSentence.Http.HttpGet("https://v1.hitokoto.cn/?encode=text");
+            } while (hitokoto.Length > 15);
+
+            int thisWidth = this.Width;
+            this.Width = hitokoto.Length * 37;
+            this.Location = new Point(this.Location.X + thisWidth / 2 - this.Width / 2, this.Location.Y);
+
+            Txt_hitokoto.Text = hitokoto;
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                MessageBox.Show("F2启动或关闭置顶\r\nF3调整窗体位置\r\nF4调整文本样式\r\nF5调整文本颜色\r\nF6调整背景颜色", "帮助");
+            }
+            if (e.KeyCode == Keys.F2)
+            {
+                this.TopMost = !this.TopMost;
+            }
+            if (e.KeyCode == Keys.F3)
+            {
+                if (this.FormBorderStyle == System.Windows.Forms.FormBorderStyle.Sizable)
+                {
+                    this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    this.TransparencyKey = System.Drawing.Color.Gray;
+                    this.BackColor = System.Drawing.Color.Gray;
+                }
+                else
+                {
+                    this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+                    this.TransparencyKey = System.Drawing.Color.Transparent;
+                }
+            }
+            if (e.KeyCode == Keys.F4)
+            {
+                this.Fd_Style.Font = Txt_hitokoto.Font;
+
+                this.Fd_Style.ShowDialog();
+                Txt_hitokoto.Font = this.Fd_Style.Font;
+            }
+            if (e.KeyCode == Keys.F5)
+            {
+                this.Cd_color.Color = Txt_hitokoto.ForeColor;
+
+                this.Cd_color.ShowDialog();
+                Txt_hitokoto.ForeColor = this.Cd_color.Color;
+            }
+            if (e.KeyCode == Keys.F6)
+            {
+                this.Cd_color.Color = this.BackColor;
+
+                this.Cd_color.ShowDialog();
+                this.BackColor = this.Cd_color.Color;
+                this.TransparencyKey = this.Cd_color.Color;
+            }
+        }
+    }
+}
